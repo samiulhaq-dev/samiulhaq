5f4d47c2cb66602f5809e5f00fa069f9d2bb4b22
sami-ul-haq <samiulhaq1810@gmail.com>
2023-07-07 14:36:56 +0500

    added animation 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,21 +18,19 @@ const Projects = () => {
         <SectionHeader title={"projects"} />
       </InViewAnimate>
       <InViewAnimate>
-
-      <div className="projects-list grid grid-cols-1 gap-x-5 gap-y-10 my-14 md:grid-cols-2 lg:grid-cols-3 ">
-        {data?.slice(0, 9).map((project) => (
-          <ProjectCard project={project} key={project.id} />
+        <div className="projects-list grid grid-cols-1 gap-x-5 gap-y-10 my-14 md:grid-cols-2 lg:grid-cols-3 ">
+          {data?.slice(0, 9).map((project) => (
+            <ProjectCard project={project} key={project.id} />
           ))}
-      </div>
-          </InViewAnimate>
-          <InViewAnimate>
-
-      <div className="projects-footer text-center">
-        <button className="text-sm px-4 py-2 text-white border border-[#C778DD] hover:bg-[#C778DD33] duration-200">
-          View More Project ~~&gt;
-        </button>
-      </div>
-          </InViewAnimate>
+        </div>
+      </InViewAnimate>
+      <InViewAnimate>
+        <div className="projects-footer text-center">
+          <button className="text-sm px-4 py-2 text-white border border-[#C778DD] hover:bg-[#C778DD33] duration-200">
+            View More Project ~~&gt;
+          </button>
+        </div>
+      </InViewAnimate>
     </div>
   );
 };
