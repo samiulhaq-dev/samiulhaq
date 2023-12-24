06d32d11897a9a76b2013c6fc29a8886c33c5866
Sami ul Haq <samiulhaq1899@gmail.com>
2023-12-24 15:02:24 +0500

    Updated Number

--- src/components/ContactHome.jsx
+++ src/components/ContactHome.jsx
@@ -12,12 +12,12 @@ const ContactHome = () => {
       </div>
       <div className="contact-content flex justify-between flex-col gap-10 sm:gap-5 sm:flex-row pt-14">
         <div className="left flex-1">
-          <p className="text-greyish">I'm interested in freelance apportunities.<br/><br/> However if you have any other request, don't hesitate to contact me</p>
+          <p className="text-greyish">I'm interested in freelance opportunities.<br/><br/> However if you have any other requests, don't hesitate to contact me</p>
         </div>
         <div className="right flex-1 sm:text-right">
           <div className="mxg-box inline-block border border-greyish p-4 hover:bg-[#C778DD33]">
               <h3 className="text-white font-semibold mb-5">Message me here</h3>
-              <a href="tel:923133005014" className="text-greyish block">+ 92 313 300 5014</a>
+              <a href="tel:923132005014" className="text-greyish block">+ 92 313 200 5014</a>
               <a href="mailto:samiulhaq.dev@gmail.com" className="text-greyish block">samiulhaq.dev@gmail.com</a>
           </div>
         </div>
